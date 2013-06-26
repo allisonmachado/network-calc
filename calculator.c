3f46e093d156d68e72d9a8598609b8d4c8851d89
allison <aallison93@hotmail.com>
2013-06-26 01:26:16 -0300

    calculado o endereço de rede

--- calculator.c
+++ calculator.c
@@ -3,19 +3,36 @@
 
 int main()
 {
-	unsigned int number = 10;
+	unsigned int network_address = 0;
+	unsigned int broadcast = 0;
+	unsigned int host_max = 0;
+	unsigned int host_min = 0;
+
+	//É necessário tratar os valores abaixo
+	unsigned int number = 168430592;
+	unsigned int net_mask = 4294967292;
+	unsigned int qtd_bt_host = 2;
+
 	unsigned int x = 2;
 	unsigned int bit;
+
+	network_address = number & net_mask; 
+
+	printf("Network address = %d\n", network_address);
 	
+	// Setando um bit
 	number |= 1 << x;
 	printf("%d\n",number);
 
+	// Limpando um bit
 	number &= ~(1 << x);
 	printf("%d\n",number);
 
+	// Trocando um bit
 	number ^= 1 << x;
 	printf("%d\n",number);
 
+	// Checando o bit
 	bit = number & (1 << x);
 	printf("%d\n",bit);
 }
