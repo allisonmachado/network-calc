bf60c1f47618a20040120975586770290520534d
allison <aallison93@hotmail.com>
2013-06-26 19:06:04 -0300

    calculado endereço de broadcast, host min e max

--- calculator.c
+++ calculator.c
@@ -13,26 +13,33 @@ int main()
 	unsigned int net_mask = 4294967292;
 	unsigned int qtd_bt_host = 2;
 
-	unsigned int x = 2;
-	unsigned int bit;
+	//unsigned int x = 2;
+	//unsigned int bit;
 
 	network_address = number & net_mask; 
+	broadcast = ~(net_mask);
+	broadcast = broadcast + network_address;
+	host_min = network_address + 1;
+	host_max = broadcast - 1;
 
 	printf("Network address = %d\n", network_address);
+	printf("Broadcast address = %d\n", broadcast);
+	printf("Host Min = %d\n", host_min);
+	printf("Host Max = %d\n", host_max);
 	
 	// Setando um bit
-	number |= 1 << x;
-	printf("%d\n",number);
+	//number |= 1 << x;
+	//printf("%d\n",number);
 
 	// Limpando um bit
-	number &= ~(1 << x);
-	printf("%d\n",number);
+	//number &= ~(1 << x);
+	//printf("%d\n",number);
 
 	// Trocando um bit
-	number ^= 1 << x;
-	printf("%d\n",number);
+	//number ^= 1 << x;
+	//printf("%d\n",number);
 
 	// Checando o bit
-	bit = number & (1 << x);
-	printf("%d\n",bit);
+	//bit = number & (1 << x);
+	//printf("%d\n",bit);
 }
