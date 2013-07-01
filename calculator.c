ed6849f3888e1c3fb861b09f2a821c98157d1301
allison <aallison93@hotmail.com>
2013-06-30 21:25:21 -0300

    adicionada função para imprimir endereços de rede

--- calculator.c
+++ calculator.c
@@ -3,6 +3,25 @@
 
 #define BUFFER 100
 
+unsigned int ip_number = 0;
+unsigned int mask_number = 0, number_of_mask_bits;
+
+void print_ip_address(int * number_addr)
+{
+    unsigned char * raw_ptr;
+    int i, aux_int;
+    raw_ptr = (unsigned char *) number_addr;
+
+    for (i = 3; i > -1; i--)
+    {
+        aux_int = raw_ptr[i];
+        printf("%d", aux_int);
+        if(i != 0)
+            printf(".");
+    }
+    printf("\n");
+}
+
 void binary_print(unsigned int number)
 {
     unsigned int mask = 0xff000000; // máscara para o último byte
@@ -22,80 +41,70 @@ void binary_print(unsigned int number)
                 printf("0");
             byte <<= 1;
         }
-        mask >>= 8; // ajusta a máscara para pegar o próximo byte
-        shift -= 8; // ajusta o shift para pegar o próximo byte
+        mask >>= 8; // ashiftusta a máscara para pegar o próximo byte
+        shift -= 8; // ashiftusta o shift para pegar o próximo byte
         
     }
 }
 
 void handle_input()
 {
-	char string[BUFFER] = "10.10.12.0/30";
-	unsigned int shift = 0;
-	unsigned int ip_number = 0;
-	unsigned int mask_number = 0, number_of_bits;
-	unsigned int i, j, temp = 0;
+    char string[BUFFER] = "10.10.12.0/30";
+    unsigned int i, shift, temp = 0;
+    
+    shift = 24;
+    ip_number = (atoi(&string[0]) << shift);
+    shift -= 8;
 
-	j = 16;
-	
-	ip_number = (atoi(&string[0]) << 24);
+    for(i = 0; string[i] != 0; i++)
+    {
+    	if(string[i] == 46) // Verifica se o caractere é um ponto e pega o valor númérico após o ponto
+    	{
+    		ip_number += (atoi(&string[i + 1]) << shift);
+    		shift -= 8;
+    	}
+    }
+    
+    for(i = 0; string[i] != 0; i++)
+    {
+    	if(string[i] == 47)
+    	{
+    		number_of_mask_bits += atoi(&string[i + 1]);
+    	}
+    }
 
-	for(i = 0; string[i] != 0; i++)
-	{
-		if(string[i] == 46) // Verifica se o caractere é um ponto e pega o valor númérico após o ponto
-		{
-			//printf("string: %d\n", atoi(&string[i + 1]));
-			ip_number += (atoi(&string[i + 1]) << j);
-			j -= 8;
-		}
-	}
-	
-	for(i = 0; string[i] != 0; i++)
-	{
-		if(string[i] == 47)
-		{
-			number_of_bits += atoi(&string[i + 1]);
-		}
-	}
-
-    printf("\n");
-    printf("\n");
-
-	for(i = 31; i > (31 - number_of_bits); i--)
-	{
+    for(i = 31; i > (31 - number_of_mask_bits); i--)
+    {
         temp = (1 << i);
         mask_number += temp;
-	}
-
-	printf("%d\n",ip_number);
-	printf("%d\n",number_of_bits);
-	printf("%d\n",mask_number);
+    }
 }
 
 int main()
 {
-	// Declarações
-	unsigned int network_address = 0;
-	unsigned int broadcast = 0;
-	unsigned int host_min = 0;
-	unsigned int host_max = 0;
+    // Declarações
+    unsigned int network_address = 0;
+    unsigned int broadcast = 0;
+    unsigned int host_min = 0;
+    unsigned int host_max = 0;
 
-	// É necessário tratar os valores abaixo
-	unsigned int input_address = 168430592;
-	unsigned int net_mask = 4294967292;
-	unsigned int qtd_bt_host = 2;
+    handle_input();
 
-	// Calculando valores
-	network_address = input_address & net_mask; 
-	broadcast = ~(net_mask);
-	broadcast = broadcast + network_address;
-	host_min = network_address + 1;
-	host_max = broadcast - 1;
+    // Calculando valores
+    network_address = ip_number & mask_number; 
+    broadcast = ~(mask_number);
+    broadcast = broadcast + network_address;
+    host_min = network_address + 1;
+    host_max = broadcast - 1;
 
-	// Imprimindo valores
-	printf("Network address = %d\n", network_address);
-	printf("Broadcast address = %d\n", broadcast);
-	printf("Host Min = %d\n", host_min);
-	printf("Host Max = %d\n", host_max);
-	handle_input();
+    // Imprimindo valores
+    printf("Network address = %d\n", network_address);
+    print_ip_address(&network_address);
+    printf("Broadcast address = %d\n", broadcast);
+    print_ip_address(&broadcast);
+    printf("Host Min = %d\n", host_min);
+    print_ip_address(&host_min);
+    printf("Host Max = %d\n", host_max);
+    print_ip_address(&host_max);
+    print_ip_address(&mask_number);
 }
